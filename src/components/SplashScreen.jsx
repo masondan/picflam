11cfb4f6acd280edf24bc62dd6fb355e3a845e95
Dan Mason <dan@danmason.co.uk>
2026-01-15 08:17:14 +0100

    fix: adjust splash screen colour, text

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { LuDrum } from 'react-icons/lu';
 import './SplashScreen.css';
 
 function SplashScreen({ onGetStarted }) {
@@ -8,12 +7,12 @@ function SplashScreen({ onGetStarted }) {
       <div className="splash-content">
         <div className="splash-main-content">
           <img src="/picflam-logo.png" alt="PicFlam Logo" className="splash-logo" />
-          <p className="splash-tagline">Add text to images for Stories and carousels. Create quotes. Free.</p>
+          <p className="splash-tagline">Add text to images for Stories<br />and carousels. Create quotes</p>
           <button className="splash-button" onClick={onGetStarted}>
             Get Started
           </button>
         </div>
-        <p className="splash-credit">Made with <LuDrum /> by Dan Mason</p>
+
       </div>
     </div>
   );
