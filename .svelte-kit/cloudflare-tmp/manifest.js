06a9604c73027191875dd8c266c672ef805bef89
Dan Mason <dan@danmason.co.uk>
2026-02-08 14:24:48 +0000

    fix: cloudflare output directory change

---
+++ .svelte-kit/cloudflare-tmp/manifest.js
@@ -0,0 +1,57 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["icons/icon-add.svg","icons/icon-ai-slider.svg","icons/icon-ai.svg","icons/icon-align-center.svg","icons/icon-align-left.svg","icons/icon-align-right.svg","icons/icon-bold.svg","icons/icon-close.svg","icons/icon-collapse.svg","icons/icon-compare.svg","icons/icon-copy.svg","icons/icon-crop.svg","icons/icon-custom.svg","icons/icon-design.svg","icons/icon-erase.svg","icons/icon-expand.svg","icons/icon-export.svg","icons/icon-fill.svg","icons/icon-fit.svg","icons/icon-flip-horizontal.svg","icons/icon-handle.svg","icons/icon-horizontal.svg","icons/icon-layer-above.svg","icons/icon-layer-below.svg","icons/icon-lock.svg","icons/icon-no-wrap.svg","icons/icon-none.svg","icons/icon-nudge-down.svg","icons/icon-nudge-left.svg","icons/icon-nudge-right.svg","icons/icon-nudge-up.svg","icons/icon-quote-serif.svg","icons/icon-quote-slab.svg","icons/icon-redo.svg","icons/icon-reset.svg","icons/icon-resize.svg","icons/icon-restore.svg","icons/icon-rotate.svg","icons/icon-search.svg","icons/icon-square.svg","icons/icon-startagain.svg","icons/icon-undo.svg","icons/icon-unlock.svg","icons/icon-upload.svg","icons/icon-vertical.svg","icons/icon-wrap.svg","logos/favicon.ico","logos/favicon.png","logos/logo-picflam-gen.png","logos/logo-picflam-logotype.png","logos/logo-picflam-maskable.png","logos/logo-picflam-og.png","logos/logo-picflam-touch.png","manifest.json","robots.txt","template-previews/template-1.png","template-previews/template-2.png","template-previews/template-3.png","template-previews/template-4.png","template-previews/template-5.png"]),
+	mimeTypes: {".svg":"image/svg+xml",".png":"image/png",".json":"application/json",".txt":"text/plain"},
+	_: {
+		client: {start:"_app/immutable/entry/start.V5H2fwd_.js",app:"_app/immutable/entry/app.DIup4749.js",imports:["_app/immutable/entry/start.V5H2fwd_.js","_app/immutable/chunks/B3scH4SM.js","_app/immutable/chunks/D7DYR7IZ.js","_app/immutable/chunks/-4YIK25C.js","_app/immutable/entry/app.DIup4749.js","_app/immutable/chunks/B7DFisc8.js","_app/immutable/chunks/D7DYR7IZ.js","_app/immutable/chunks/-4YIK25C.js","_app/immutable/chunks/DgmTrjkO.js","_app/immutable/chunks/BOM9bC5J.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		nodes: [
+			__memo(() => import('../output/server/nodes/0.js')),
+			__memo(() => import('../output/server/nodes/1.js')),
+			__memo(() => import('../output/server/nodes/2.js'))
+		],
+		remotes: {
+			
+		},
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			},
+			{
+				id: "/api/face-enhance",
+				pattern: /^\/api\/face-enhance\/?$/,
+				params: [],
+				page: null,
+				endpoint: __memo(() => import('../output/server/entries/endpoints/api/face-enhance/_server.js'))
+			},
+			{
+				id: "/api/upscale",
+				pattern: /^\/api\/upscale\/?$/,
+				params: [],
+				page: null,
+				endpoint: __memo(() => import('../output/server/entries/endpoints/api/upscale/_server.js'))
+			}
+		],
+		prerendered_routes: new Set([]),
+		matchers: async () => {
+			
+			return {  };
+		},
+		server_assets: {}
+	}
+}
+})();
+
+export const prerendered = new Set([]);
+
+export const base_path = "";
---
+++ .svelte-kit/cloudflare/_app/immutable/chunks/BIzliudy.js
@@ -0,0 +1,1 @@
+import{h as a}from"./D7DYR7IZ.js";a();
