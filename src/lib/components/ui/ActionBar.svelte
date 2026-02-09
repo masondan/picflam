94ae38e22b9a50a8618ea73f642f06be5d8cadde
Dan Mason <dan@danmason.co.uk>
2026-02-09 06:07:03 +0000

    fix: edit tab action bar spacing

--- src/lib/components/ui/ActionBar.svelte
+++ src/lib/components/ui/ActionBar.svelte
@@ -58,8 +58,8 @@
 		display: flex;
 		align-items: center;
 		justify-content: space-between;
-		padding: var(--space-2) 0;
-		margin-bottom: var(--space-0.5);
+		padding: var(--space-1) 0;
+		margin-bottom: var(--space-0.0);
 	}
 	
 	.action-group {
