536b97add9112ead39508b79d67fec6511f3f15c
Dan Mason <dan@danmason.co.uk>
2026-03-04 20:56:37 +0000

    fix: add admin reset for image gen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
 	import '$lib/styles/global.css';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const params = new URLSearchParams(window.location.search);
+		if (params.has('reset')) {
+			localStorage.setItem('picflam_gen_count', '0');
+			window.history.replaceState({}, '', window.location.pathname);
+		}
+	});
 </script>
 
 <div class="app">
